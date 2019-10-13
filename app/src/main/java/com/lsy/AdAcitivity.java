090ce8fc796b061c7c80d277ca0a9122c8555546
ZsxhLyqz <H867331878@gmailcom>
2019-10-13 18:50:24 +0800

    广告跳转完成

---
+++ app/src/main/java/com/lsy/AdAcitivity.java
@@ -0,0 +1,69 @@
+package com.lsy;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.SystemClock;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.lsy.R;
+
+public class AdAcitivity extends AppCompatActivity {
+    TextView tv_count;
+    Handler handler=new Handler();
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_ad);
+
+        tv_count =findViewById(R.id.tv_count);
+
+        /*new Thread(){
+            @Override
+            public void run() {
+                super.run();
+                //运行新线程
+            }
+        }.start();*/
+
+       new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+            for(int i=5;i>=0;i--){
+                    SystemClock.sleep(1000);
+                    //修改UI的操作必须放在主线程
+                final int count=i;
+                //切换回主线程
+                /*runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        tv_count.setText("点击跳转"+count);
+                    }
+                })*/
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        tv_count.setText("点击跳转 "+count);
+                    }
+                });
+
+                }
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    Intent intent=new Intent(AdAcitivity.this,MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            });
+            }
+        }).start();
+
+    }
+}
---
+++ app/src/main/java/com/lsy/SplashActivity.java
@@ -0,0 +1,46 @@
+package com.lsy;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        ImageView LoadingView = findViewById(R.id.iv_loading);
+
+        Animation animation= AnimationUtils.loadAnimation(SplashActivity.this,R.anim.splash_loading);
+
+        animation.setFillAfter(true);
+
+        animation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+
+                Intent intent=new Intent(SplashActivity.this,AdAcitivity.class);
+                startActivity(intent);
+                finish();
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+        LoadingView.startAnimation(animation);
+    }
+}
