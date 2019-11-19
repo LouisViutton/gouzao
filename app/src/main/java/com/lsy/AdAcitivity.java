2d9e38a4ed6c912958e98a2cb8800132532b7c38
huatec-1 <H867331878@gmail.com>
2019-11-19 11:05:00 +0800

    图片加载

--- app/src/main/java/com/lsy/AdAcitivity.java
+++ app/src/main/java/com/lsy/AdAcitivity.java
@@ -7,10 +7,15 @@ import android.os.SystemClock;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 
+import com.bumptech.glide.Glide;
 import com.lsy.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class AdAcitivity extends AppCompatActivity {
     TextView tv_count;
@@ -18,11 +23,20 @@ public class AdAcitivity extends AppCompatActivity {
 
     Boolean isStop;
 
+    @BindView(R.id.tv_ad)
+    ImageView tv_ad;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_ad);
+
+        ButterKnife.bind(this);
+
+        //加载图片
+        Glide.with(this)
+                .load("https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1574140080796&di=51942524e1a17dcef3424430b812d53b&imgtype=0&src=http%3A%2F%2Fd.hiphotos.baidu.com%2Fzhidao%2Fwh%253D450%252C600%2Fsign%3D17eee5b3820a19d8cb568c0106caaebd%2Ffaf2b2119313b07e11d1ec0a0ed7912397dd8c40.jpg")
+                .into(tv_ad);
 
         tv_count = findViewById(R.id.tv_count);
 
